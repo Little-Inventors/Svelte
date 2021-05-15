bedce9c76016d6a8091752da1cf337dbd35775da
irene <irenekurien01@gmail.com>
2021-05-15 19:58:27 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import TextInput from "./components/TextInput.svelte";
+  export let name;
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <TextInput id="name" type="text" label="Name"/>
+  <TextInput id="email" type="email" label="email"/>
+  <TextInput id="contactno" type="number" label="contactno"/>
+  <TextInput id="password" type="password" label="password"/>
+  <TextInput id="date" type="date" label="date"/>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/TextInput.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let id;
+    export let label;
+    export let type;
+</script>
+
+<label for="{id}">{label}</label>
+<input type="{type}" id="{id}" />
