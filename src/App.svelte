028501bdd47b92ec9eac041d51b2620a75edb528
irene <irenekurien01@gmail.com>
2021-05-17 09:46:24 +0530

    button handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,38 @@
 <script>
-  let firstName = "Irene";
-  let secondName = "Anna";
-  let fullName = "";
-  let color = "red";
-
-  $: fullName = `${firstName} ${secondName}`;
-  $: {
-    console.log(color);
-    console.log(fullName);
-  }
+	let name = 'Irene';
+  let color = 'black';
+  const handleClick = () => {
+    color = 'orange';
+  };
+  const handleInput = (e) => {
+    color = e.target.value;
+  };
 </script>
 
 <main>
-  <p>{fullName} -- {color}</p>
-  <input bind:value={firstName} />
-  <input bind:value={secondName} />
-
-  <!-- <input on:input={handleInput} value={color}/> -->
-
-  <input bind:value={color} />
+	<h1>{name}</h1>
+	<p style="color: {color}">{color} belt</p>
+  <button on:click={handleClick}>update belt colour</button>
+  <!-- <input type="text" on:input={handleInput} value={color}> -->
+  <input type="text" bind:value={color}>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
